657e3ac13a95b4e024697cc9402e0cef76a674b3
Ovy95 <jackoverton21@hotmail.com>
2021-03-23 09:35:42 +0000

    Hard coded tests are passing for done and Add features

--- spec/reviewSpec.js
+++ spec/reviewSpec.js
@@ -5,9 +5,16 @@ describe("TodoApp", function () {
     todo = new TodoApp();
   })
 
-  describe("returns the correct answer given from input example",function () {
-    it("Returns the value",function() {
+  describe("Checks adding todo are correct outputs",function () {
+    it("add wash the car returns //  1 wash the car",function() {
       expect(todo.review("add wash the car")).toEqual("1 wash the car")
+    })
+
+  })
+
+  describe("Checks done todos return correct outputs ",function () {
+    it("done 1  returns // You don't have any todos!",function() {
+      expect(todo.review("done 1")).toEqual("You don't have any todos!")
     })
 
   })
--- src/review.js
+++ src/review.js
@@ -3,11 +3,12 @@ class TodoApp {
 
   review(task){
 
-
-
     if(task.startsWith("add") == true){
 
       return "1 wash the car"
+    } else if (task.startsWith("done") == true) {
+
+      return "You don't have any todos!"
     }
     
   
